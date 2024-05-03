844bdb97980f37f5cffea2b9121ee33d8ffed851
PC LENOVO <arfaprapen@gmail.com>
2024-05-03 14:41:55 +0700

    first commit

---
+++ try.php
@@ -0,0 +1,6 @@
+<body style="background-color:yellowgreen;">
+    <h1>This is a heading</h1>
+    <p>This is a paragraph.</p>
+    </body>
+    <!--mengubah background ke warna yellowgreen dengan atribut style-->
+
